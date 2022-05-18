e0460110fec8feb173107ff4465a2b6fc9cfaaaa
Hossein <djalicrt@gmail.com>
2022-05-18 19:40:58 +0430

    Second Part - Animation + Animator + EulerAngles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,16 +10,43 @@ public class Player : MonoBehaviour
     public float jump = 5;
 
     private bool isgrounded = false;
+
+    private Animator anim;
+    private Vector3 rotation;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rotation = transform.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
         float direction = Input.GetAxis("Horizontal");
-        transform.Translate(Vector2.right * speed * direction * Time.deltaTime);
+
+        if (direction != 0) {
+            anim.SetBool("IsRunning" , true);
+        } else {
+            anim.SetBool("IsRunning" , false);
+        }
+
+        if (direction < 0) {
+            transform.eulerAngles = rotation - new Vector3(0,180,0);
+            transform.Translate(Vector2.right * speed * -direction * Time.deltaTime);
+        }
+        if (direction > 0) {
+            transform.eulerAngles = rotation;
+            transform.Translate(Vector2.right * speed * direction * Time.deltaTime);
+        }
+
+        if (isgrounded == false) {
+            anim.SetBool("IsJumping", true);
+        } else {
+            anim.SetBool("IsJumping", false);
+        }
+
+        
 
         if (Input.GetKeyDown(KeyCode.Space) && isgrounded) {
             rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
